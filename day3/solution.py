161d2865d278adfc77784dbfadfe0204168bede3
d.rutkovsky <derivz@gmail.com>
2022-12-04 08:03:58 +0200

    day4

--- day3/solution.py
+++ day3/solution.py
@@ -10,13 +10,9 @@ for line in lines:
 print("Part 1: ", score)
 
 score = 0
-for i in range(len(lines) // 3):
-    common = (
-        set(lines[i * 3 + 0])
-        .intersection(set(lines[i * 3 + 1]))
-        .intersection(set(lines[i * 3 + 2]))
-    )
-    letter = common.pop()
-    score += ord(letter) - (38 if letter.isupper() else 96)
+chuncks = [lines[i:i + 3] for i in range(0, len(lines), 3)]
+for chunck in chuncks:
+    common_letter = set(chunck[0]).intersection(*chunck[1:]).pop()
+    score += ord(common_letter) - (38 if common_letter.isupper() else 96)
 
 print("Part 2: ", score)
