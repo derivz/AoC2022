401f1df9d10dceeaa35f30c9ee70be26f8ffb18d
d.rutkovsky <derivz@gmail.com>
2022-12-11 07:46:47 +0200

    day 11 part 2

--- day11/solution.py
+++ day11/solution.py
@@ -1,3 +1,5 @@
+from math import lcm
+
 with open('./input.txt') as f:
     monkeyLines = f.read().strip().split('\n\n')
 
@@ -14,12 +16,16 @@ class Monkey:
         self.inspections = 0
 
     def __repr__(self):
-        return f'Monkey({self.index}, {self.inspections})'
+        return f'Monkey({self.index}, {self.inspections}, {self.items})'
 
-    def throwItems(self):
+    def throwItems(self, divisor=None):
         while self.items:
             item = self.items.pop(0)
-            item = self.op(item) // 3
+            item = self.op(item)
+            if divisor is None:
+                item //= 3
+            else:
+                item %= divisor
             self.inspections += 1
             yield (
                 self.posMonkey if item % self.divisor == 0 else self.negMonkey,
@@ -29,14 +35,23 @@ class Monkey:
 
 monkeys = [Monkey(line) for line in monkeyLines]
 for round in range(20):
-    print(f"Round {round + 1}")
     for monkey in monkeys:
         for monkeyIndex, item in monkey.throwItems():
             monkeys[monkeyIndex].items.append(item)
-    print()
 
 
 sortedMonkeys = sorted(monkeys, key=lambda monkey: monkey.inspections, reverse=True)
 print("Part 1:", sortedMonkeys[0].inspections * sortedMonkeys[1].inspections)
 
-print("Part 2:")
+
+monkeys = [Monkey(line) for line in monkeyLines]
+common_divisor = lcm(*[monkey.divisor for monkey in monkeys])
+for round in range(10000):
+    for monkey in monkeys:
+        for monkeyIndex, item in monkey.throwItems(common_divisor):
+            monkeys[monkeyIndex].items.append(item)
+
+
+sortedMonkeys = sorted(monkeys, key=lambda monkey: monkey.inspections, reverse=True)
+print("Part 2:", sortedMonkeys[0].inspections * sortedMonkeys[1].inspections)
+
